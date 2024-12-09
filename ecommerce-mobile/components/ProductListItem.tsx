c5f0b75e5b5880351ad40222b1da499666d3d67b
adhayan002 <adhayan@gmail.com>
2024-12-09 23:31:19 +0530

    Initiated Gluestack Ui in the ecommerce project

--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -1,7 +1,16 @@
-import { Text } from 'react-native';
+import { ShoppingCart } from 'lucide-react-native';
+import { Text, View } from 'react-native';
 import { ProductItem } from '../lib/interface';
+import { Button, ButtonIcon, ButtonText } from './ui/button';
+
 export function ProductListItem({ product }: { product: ProductItem }) {
   return (
-    <Text style={{ fontSize: 30 }}>{product.name}</Text>
+    <View className='flex items-center'>
+      <Text style={{ fontSize: 20 }}>{product.name}</Text>
+      <Button size="md" className='w-32'>
+        <ButtonIcon as={ShoppingCart}/>
+        <ButtonText className='text-sm'>${product.price}</ButtonText>
+      </Button>
+    </View>
   )
 }
